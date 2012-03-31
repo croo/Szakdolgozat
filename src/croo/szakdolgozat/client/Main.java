55509c031a1c23eb9d7e9f5a334c2eaca83bfe84
Croo <croohmanoor@gmail.com>
2012-03-31 20:26:40 +0200

    Removed the experimenting code from main and binded the TravelMap view to its div tag.

--- src/croo/szakdolgozat/client/Main.java
+++ src/croo/szakdolgozat/client/Main.java
@@ -2,108 +2,23 @@ package croo.szakdolgozat.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Style.Unit;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.maps.client.InfoWindowContent;
-import com.google.gwt.maps.client.MapWidget;
-import com.google.gwt.maps.client.control.LargeMapControl;
-import com.google.gwt.maps.client.event.MapClickHandler;
-import com.google.gwt.maps.client.geom.LatLng;
-import com.google.gwt.maps.client.overlay.Marker;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DockLayoutPanel;
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
+import com.google.web.bindery.event.shared.EventBus;
+import com.google.web.bindery.event.shared.SimpleEventBus;
 
-import croo.szakdolgozat.client.stubs.QueryService;
-import croo.szakdolgozat.client.stubs.QueryServiceAsync;
-import croo.szakdolgozat.shared.Coordinates;
+import croo.szakdolgozat.client.view.TravelMap;
 
-public class Main implements EntryPoint, MapClickHandler, ClickHandler
+public class Main implements EntryPoint
 {
-	private final QueryServiceAsync reverseService = GWT.create(QueryService.class);
-	private TextBox inputField;
-	private Button sendButton;
-	private HTML htmlBlock;
-	private MapWidget map;
 
 	/**
 	 * Entry Point
 	 */
 	public void onModuleLoad()
 	{
-		inputField = new TextBox();
-		sendButton = new Button("Send");
-		htmlBlock = new HTML();
+		EventBus eventBus = GWT.create(SimpleEventBus.class);
 
-		sendButton.addStyleName("sendButton");
-
-		RootPanel.get("input").add(inputField);
-		RootPanel.get("button").add(sendButton);
-		RootPanel.get("label").add(htmlBlock);
-
-		inputField.setFocus(true);
-		inputField.selectAll();
-
-		sendButton.addClickHandler(this);
-	}
-
-	private void buildUi()
-	{
-		LatLng budapest = LatLng.newInstance(47.309, 19.500);
-		map = new MapWidget(budapest, 2);
-		map.setSize("640px", "480px");
-		map.addControl(new LargeMapControl());
-		map.setZoomLevel(7);
-		map.addOverlay(new Marker(budapest));
-		map.getInfoWindow().open(map.getCenter(), new InfoWindowContent("Middle of Europe... more or less."));
-		final DockLayoutPanel dock = new DockLayoutPanel(Unit.PX);
-		map.setScrollWheelZoomEnabled(true);
-		map.setContinuousZoom(true);
-		dock.addNorth(map, 500);
-		RootPanel.get("map").add(dock);
-		map.addMapClickHandler(this);
-	}
-
-	private void locateInputOnMap(String place)
-	{
-		sendButton.setEnabled(false);
-		reverseService.query(place, new AsyncCallback<Coordinates>() {
-			public void onFailure(Throwable caught)
-			{
-				htmlBlock.setHTML(caught.getMessage());
-				GWT.log(caught.getMessage());
-				sendButton.setEnabled(true);
-			}
-
-			public void onSuccess(Coordinates result)
-			{
-				if (result != null)
-				{
-					htmlBlock.setHTML(result.getLatitude() + ", " + result.getLongitude());
-					sendButton.setEnabled(true);
-					map.setCenter(LatLng.newInstance(result.getLatitude(), result.getLongitude()));
-				} else
-				{
-					htmlBlock.setHTML("This place cannot be found in the database. Please try another one.");
-				}
-			}
-		});
-	}
-
-	@Override
-	public void onClick(MapClickEvent event)
-	{
-		htmlBlock.setHTML("The coords you clicked on: " + String.valueOf(event.getLatLng().getLatitude()) + " , "
-				+ String.valueOf(event.getLatLng().getLongitude()));
-	}
-
-	@Override
-	public void onClick(ClickEvent event)
-	{
-		locateInputOnMap(inputField.getText());
+		TravelMap map = new TravelMap(eventBus);
+		RootPanel.get("map").add(map);
 	}
 }
