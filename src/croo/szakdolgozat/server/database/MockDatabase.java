915e15f1a17bef1206aec83fc95bd565605df8b7
Croo <croohmanoor@gmail.com>
2012-04-01 22:13:44 +0200

    Now the implementation of the mock database makes sure that the Routeway data is sorted. This is good here: the real database will depend on the order of the data and will probably not have to sort it.

--- src/croo/szakdolgozat/server/database/MockDatabase.java
+++ src/croo/szakdolgozat/server/database/MockDatabase.java
@@ -1,6 +1,8 @@
 package croo.szakdolgozat.server.database;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,7 +15,7 @@ public class MockDatabase implements Database
 
 	public MockDatabase()
 	{
-		db.put("budapest", new Coordinate(47.309, 19.500));
+		db.put("budapest", new Coordinate(47.49841, 19.04076));
 		db.put("esztergom", new Coordinate(47.7776069, 18.7435935));
 	}
 
@@ -30,8 +32,21 @@ public class MockDatabase implements Database
 		routeway.add(db.get(startTown));
 		routeway.add(new Coordinate(47.500001, 18.800001));
 		routeway.add(new Coordinate(47.600001, 18.990001));
-		routeway.add(new Coordinate(47.400001, 19.20001));
+		routeway.add(new Coordinate(47.550001, 19.01301));
 		routeway.add(db.get(endTown));
+		sortCoordinatesInRouteway(routeway);
+
 		return new Route(startTown, endTown, routeway);
+	}
+
+	private void sortCoordinatesInRouteway(ArrayList<Coordinate> routeway)
+	{
+		Collections.sort(routeway, new Comparator<Coordinate>() {
+			@Override
+			public int compare(Coordinate a, Coordinate b)
+			{
+				return a.getLongitude().compareTo(b.getLongitude());
+			}
+		});
 	}
 }
