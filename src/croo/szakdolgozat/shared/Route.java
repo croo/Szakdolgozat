e51730b3c52d64e7c8d8d004845d778d015c884a
Croo <croohmanoor@gmail.com>
2012-04-01 15:34:45 +0200

    Created some serializable entities for communication between the client & server.

---
+++ src/croo/szakdolgozat/shared/Route.java
@@ -0,0 +1,51 @@
+package croo.szakdolgozat.shared;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Route implements Serializable
+{
+	private static final long serialVersionUID = 3955046507192507058L;
+
+	private Town startTown;
+	private Town endTown;
+	private ArrayList<Coordinates> routeway;
+
+	public Route(Town startTown, Town endTown, ArrayList<Coordinates> routeway)
+	{
+		super();
+		this.startTown = startTown;
+		this.endTown = endTown;
+		this.routeway = routeway;
+	}
+
+	public Town getStartTown()
+	{
+		return startTown;
+	}
+
+	public void setStartTown(Town startTown)
+	{
+		this.startTown = startTown;
+	}
+
+	public Town getEndTown()
+	{
+		return endTown;
+	}
+
+	public void setEndTown(Town endTown)
+	{
+		this.endTown = endTown;
+	}
+
+	public ArrayList<Coordinates> getRouteway()
+	{
+		return routeway;
+	}
+
+	public void setRouteway(ArrayList<Coordinates> routeway)
+	{
+		this.routeway = routeway;
+	}
+}
---
+++ src/croo/szakdolgozat/shared/Town.java
@@ -0,0 +1,39 @@
+package croo.szakdolgozat.shared;
+
+import java.io.Serializable;
+
+public class Town implements Serializable
+{
+	private static final long serialVersionUID = 8688979676885314458L;
+
+	private Coordinates coordinate;
+	private String name;
+
+	public Town(Coordinates coordinate, String name)
+	{
+		super();
+		this.coordinate = coordinate;
+		this.name = name;
+	}
+
+	public Coordinates getCoordinate()
+	{
+		return coordinate;
+	}
+
+	public void setCoordinate(Coordinates coordinate)
+	{
+		this.coordinate = coordinate;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+}
