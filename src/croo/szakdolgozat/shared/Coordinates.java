93af2b49a4dea261cb700e498a992c00fe7fdee7
Croo <croohmanoor@gmail.com>
2012-04-01 20:41:06 +0200

    Created JavaScriptObject getter for the Coordinates class and overrided equals, hashCode.

--- src/croo/szakdolgozat/shared/Coordinates.java
+++ src/croo/szakdolgozat/shared/Coordinates.java
@@ -1,21 +1,27 @@
 package croo.szakdolgozat.shared;
 
+import com.google.gwt.maps.client.geom.LatLng;
 import com.google.gwt.user.client.rpc.IsSerializable;
 
 public class Coordinates implements IsSerializable
 {
-	private double latitude;
-	private double longitude;
+	private Double latitude;
+	private Double longitude;
 
 	public Coordinates()
 	{
 		/* GWT RPC needs an empty no-arguments constructor */
 	}
 
-	public Coordinates(double lat, double lng)
+	public Coordinates(Double lat, Double lng)
 	{
 		latitude = lat;
 		longitude = lng;
+	}
+
+	public LatLng getCoordinateInJSO()
+	{
+		return LatLng.newInstance(latitude, longitude);
 	}
 
 	public double getLatitude()
@@ -23,7 +29,7 @@ public class Coordinates implements IsSerializable
 		return latitude;
 	}
 
-	public void setLatitude(double latitude)
+	public void setLatitude(Double latitude)
 	{
 		this.latitude = latitude;
 	}
@@ -33,8 +39,27 @@ public class Coordinates implements IsSerializable
 		return longitude;
 	}
 
-	public void setLongitude(double longitude)
+	public void setLongitude(Double longitude)
 	{
 		this.longitude = longitude;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj instanceof Coordinates) {
+			Coordinates other = (Coordinates) obj;
+			return latitude.equals(other.latitude) && longitude.equals(other.longitude);
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return latitude.hashCode() / longitude.hashCode();
 	}
 }
