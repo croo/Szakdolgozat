4fde45ff0a34e775dfe7a277a72225ee57736f02
Croo <croohmanoor@gmail.com>
2012-04-11 12:55:32 +0200

    Mock database now uses string-Town pairs instead of String-Coordinate pairs.

--- src/croo/szakdolgozat/server/database/MockDatabase.java
+++ src/croo/szakdolgozat/server/database/MockDatabase.java
@@ -2,19 +2,23 @@ package croo.szakdolgozat.server.database;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 import croo.szakdolgozat.shared.Coordinate;
 import croo.szakdolgozat.shared.Route;
+import croo.szakdolgozat.shared.Town;
 
 public class MockDatabase implements Database
 {
-	Map<String, Coordinate> db = new HashMap<String, Coordinate>();
+	HashMap<String, Town> db = new HashMap<String, Town>();
 
 	public MockDatabase()
 	{
-		db.put("budapest", new Coordinate(47.49841, 19.04076));
-		db.put("esztergom", new Coordinate(47.7776069, 18.7435935));
+		ArrayList<String> placesInEsztergom = new ArrayList<>();
+		placesInEsztergom.add("http://budapest.hu");
+		placesInEsztergom.add("http://skanzen.hu");
+
+		db.put("budapest", new Town(new Coordinate(47.49841, 19.04076), "budapest"));
+		db.put("esztergom", new Town(new Coordinate(47.7776069, 18.7435935), "esztergom", placesInEsztergom));
 	}
 
 	@Override
@@ -24,19 +28,18 @@ public class MockDatabase implements Database
 	}
 
 	@Override
-	public Route getRoute(String startTown, String endTown)
+	public Route getRoute(String startTownName, String endTownName)
 	{
 		ArrayList<Coordinate> routeway = new ArrayList<Coordinate>();
-		routeway.add(db.get(startTown));
+		routeway.add(db.get(startTownName).getCoordinate());
 		routeway.add(new Coordinate(47.500001, 18.800001));
 		routeway.add(new Coordinate(47.600001, 18.990001));
 		routeway.add(new Coordinate(47.550001, 19.01301));
-		routeway.add(db.get(endTown));
+		routeway.add(db.get(endTownName).getCoordinate());
 		// sortCoordinatesInRouteway(routeway);
 
-		return new Route(startTown, endTown, routeway);
+		return new Route(startTownName, endTownName, routeway);
 	}
-
 	// private void sortCoordinatesInRouteway(ArrayList<Coordinate> routeway)
 	// {
 	// Collections.sort(routeway, new Comparator<Coordinate>() {
