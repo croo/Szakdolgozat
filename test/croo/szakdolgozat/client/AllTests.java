984bd23bb6dbbbf2faef5fde91dfa37db8408a36
Croo <croohmanoor@gmail.com>
2012-04-11 12:58:27 +0200

    Created test suite to run all tests as one.

---
+++ test/croo/szakdolgozat/client/AllTests.java
@@ -0,0 +1,12 @@
+package croo.szakdolgozat.client;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({CoordinateTest.class, MapServiceImplTest.class, TownTest.class})
+public class AllTests
+{
+
+}
