2ffc4fa9549cbb815c808eccc9b70dbfa563f31c
Croo <croohmanoor@gmail.com>
2012-04-11 22:50:33 +0200

    First steps of listing the interesting places on the map at the destination town.

--- src/croo/szakdolgozat/client/presenter/MapManager.java
+++ src/croo/szakdolgozat/client/presenter/MapManager.java
@@ -1,11 +1,21 @@
 package croo.szakdolgozat.client.presenter;
 
+import java.util.ArrayList;
+
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.maps.client.InfoWindow;
+import com.google.gwt.maps.client.InfoWindowContent;
 import com.google.gwt.maps.client.MapWidget;
 import com.google.gwt.maps.client.control.LargeMapControl;
 import com.google.gwt.maps.client.geom.LatLng;
 import com.google.gwt.maps.client.overlay.Marker;
+import com.google.gwt.user.client.ui.Frame;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.ScrollPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
+import croo.szakdolgozat.shared.InterestingPlace;
 import croo.szakdolgozat.shared.Route;
 
 public class MapManager
@@ -19,8 +29,35 @@ public class MapManager
 
 	public void drawRoute(Route route)
 	{
+		map.clearOverlays();
 		map.addOverlay(route.getRouteWayInJSO());
-		GWT.log("Drawing the route.");
+		map.addOverlay(new Marker(route.getEndTown().getTownCoordinateInJSO()));
+
+		SimplePanel wrapper = createInterestingPlacesList(route.getEndTown().getInterestingPlaces());
+		InfoWindowContent content = new InfoWindowContent(wrapper);
+
+		Frame interestingPage = new Frame(route.getEndTown().getInterestingPlaces().get(0).getURL());
+		interestingPage.setHeight("98%");
+		interestingPage.setWidth("98%");
+		content.setMaxContent(interestingPage);
+		InfoWindow info = map.getInfoWindow();
+
+		info.open(route.getEndTown().getTownCoordinateInJSO(), content);
+		GWT.log("Drawing the route, marker.");
+	}
+
+	private SimplePanel createInterestingPlacesList(ArrayList<InterestingPlace> interestingPlaces)
+	{
+		SimplePanel wrapper = new ScrollPanel();
+
+		VerticalPanel panel = new VerticalPanel();
+		for (InterestingPlace place : interestingPlaces) {
+			HTML item = new HTML(place.getName());
+			panel.add(item);
+		}
+
+		wrapper.add(panel);
+		return wrapper;
 	}
 
 	public void addMapClickHandler(TravelMapPresenter travelMapPresenter)
@@ -43,6 +80,5 @@ public class MapManager
 		map.setScrollWheelZoomEnabled(true);
 		map.setContinuousZoom(true);
 		map.addControl(new LargeMapControl());
-		map.addOverlay(new Marker(budapest));
 	}
 }
