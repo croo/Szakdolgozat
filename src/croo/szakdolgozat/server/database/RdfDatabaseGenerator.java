5265e42c6b14e4f81ad2b2731bd0d5fd69840d35
Croo <croohmanoor@gmail.com>
2012-05-02 23:20:27 +0200

    Lets download parse and generate rdf from 70 MB random txt file #01 This code can download a zipfile and can  unzip it.

---
+++ src/croo/szakdolgozat/server/database/RdfDatabaseGenerator.java
@@ -0,0 +1,66 @@
+package croo.szakdolgozat.server.database;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+public class RdfDatabaseGenerator
+{
+	private void copyInputStream(InputStream in, OutputStream out) throws IOException
+	{
+		byte[] buffer = new byte[1024];
+		int len;
+		while ((len = in.read(buffer)) >= 0)
+			out.write(buffer, 0, len);
+		in.close();
+		out.close();
+	}
+
+	private void unZipper() throws IOException
+	{
+		saveUrl("a.html", "http://www.asdf.org");
+		try {
+			ZipFile zipFile = new ZipFile("test.zip");
+			Enumeration<? extends ZipEntry> entries = zipFile.entries();
+			while (entries.hasMoreElements()) {
+				ZipEntry entry = (ZipEntry) entries.nextElement();
+				System.out.println("Extracting file: " + entry.getName());
+				copyInputStream(zipFile.getInputStream(entry), new BufferedOutputStream(new FileOutputStream(entry.getName())));
+			}
+			zipFile.close();
+		} catch (SecurityException e) {
+			System.err.println("Security Exception. Cannot read or write :" + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	private void saveUrl(String filename, String urlString) throws IOException
+	{
+		URL google = new URL(urlString);
+		ReadableByteChannel rbc = Channels.newChannel(google.openStream());
+		FileOutputStream fos = new FileOutputStream(filename);
+		fos.getChannel().transferFrom(rbc, 0, 1 << 24);
+	}
+
+	private void downloadSource(String databaseFile) throws IOException
+	{
+		String url = SystemProperties.GetInstance().getFileLocation("mav.source.zip.url");
+		saveUrl("MavSourceData.zip", url);
+
+	}
+
+	public void generateRdfDatabase(String databaseFile)
+	{
+		downloadSource(databaseFile);
+		// TODO Auto-generated method stub
+
+	}
+}
